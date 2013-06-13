222afc5f678e91080f2e62c90d2b8cb2b1be6f9b
linfushou <linfushou@gmail.com>
2013-06-13 23:51:04 +0800

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,15 @@
+/*
+ * hello.c
+ * linfushou@gmail.com  2013-06-13
+ * first test program
+ * history:
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Hello World!\n");
+    
+    return 0;
+}
