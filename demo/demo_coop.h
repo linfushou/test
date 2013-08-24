cacf8c020c9055b1cb05164644fc97f91b1eda5d
linfushou <linfushou@ruijie.com.cn>
2013-08-25 01:00:40 +0800

    revise demo_coop.h

--- demo/demo_coop.h
+++ demo/demo_coop.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2013 CN-LAB.  All rights reserved.
- * demo_coop.c
+ * demo_coop.h
  * Original Author: linfushou@gmail.com, 2013-08-23
  * C语言面向对象编程示例
  * 说明: 将person master类 TODO
